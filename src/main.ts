224d64187a033c026be5ed3c079225fcea2854bd
Jordan Day <jordaeday22@gmail.com>
2024-08-22 15:04:12 -0500

    removed unnecessary code

--- src/main.ts
+++ src/main.ts
@@ -18,12 +18,12 @@ export default class CanvasTasksPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Canvas Tasks Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('Updating Canvas Tasks...');
-			// TODO: Add functionality to update tasks automatically
-		});
+		// // This creates an icon in the left ribbon.
+		// const ribbonIconEl = this.addRibbonIcon('dice', 'Canvas Tasks Plugin', (evt: MouseEvent) => {
+		// 	// Called when the user clicks the icon.
+		// 	new Notice('Updating Canvas Tasks...');
+		// 	// TODO: Add functionality to update tasks automatically
+		// });
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
@@ -51,16 +51,7 @@ export default class CanvasTasksPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new CanvasTasksSettingTab(this.app, this));
 	}
 
 	// onunload is called when the plugin is disabled
@@ -77,7 +68,7 @@ export default class CanvasTasksPlugin extends Plugin {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
+class CanvasTasksSettingTab extends PluginSettingTab {
 	plugin: CanvasTasksPlugin;
 
 	constructor(app: App, plugin: CanvasTasksPlugin) {
